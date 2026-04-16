a7038e6cd60a1f3923360376f30d50380ec0e9b9
Prince-Choubey <choubeyprince379@gmail.com>
2026-04-16 06:45:06 +0530

    645. Set Mismatch

--- src/Sorting/CycleSort.java
+++ src/Sorting/CycleSort.java
@@ -82,6 +82,28 @@ public class CycleSort {
         return res;
     }
 
+    public int[] findErrorNums(int[] arr) {
+        int[] res = new int[2];
+        int n = arr.length;
+        int i = 0;
+        while (i < arr.length) {
+            int idx = arr[i] - 1;
+            if (arr[i] == i + 1 || arr[idx] == arr[i]) {
+                i++;
+            } else {
+                swap(arr, i, idx);
+            }
+        }
+
+        for (i = 0; i < n; i++) {
+            if (arr[i] != i + 1) {
+                res[0] = arr[i];
+                res[1] = i + 1;
+            }
+        }
+        return res;
+    }
+
     private void swap(int[] arr, int i, int idx) {
         int temp = arr[idx];
         arr[idx] = arr[i];

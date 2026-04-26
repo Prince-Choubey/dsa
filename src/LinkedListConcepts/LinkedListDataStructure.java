07e3dfc132b62d62625d9d624e3f229c24dd1759
Prince-Choubey <choubeyprince379@gmail.com>
2026-04-26 06:39:44 +0530

    Insert At Index

--- src/LinkedListConcepts/LinkedListDataStructure.java
+++ src/LinkedListConcepts/LinkedListDataStructure.java
@@ -12,6 +12,41 @@ class Node { // user defined data type
 class Linkedlist { // user defined data structure
     Node head;
     Node tail;
+    int size;
+
+    int search(int val) {
+        if (head == null) return -1;
+        Node temp = head;
+        int idx = 0;
+        while (temp.next != null) {
+            if (temp.val == val) {
+                return idx;
+            }
+            temp = temp.next;
+            idx++;
+        }
+        return -1;
+    }
+
+    void addAtIndex(int idx, int val) {
+        if (idx < 0 || idx > size) {
+            System.out.println("Invalid Index");
+            return;
+        }
+        if (idx == 0) addAtHead(val);
+        else if (idx == size) addAtTail(val);
+        else {
+            Node newNode = new Node(val);
+            Node temp = head;
+            for (int i = 1; i < idx; i++) {
+                temp = temp.next;
+            }
+            newNode.next = temp.next;
+            temp.next = newNode;
+            size++;
+        }
+
+    }
 
     void addAtTail(int val) {
         Node temp = new Node(val);
@@ -20,23 +55,35 @@ class Linkedlist { // user defined data structure
             tail.next = temp;
             tail = temp;
         }
+        size++;
     }
 
-    void addAtHead(int val){
+    void addAtHead(int val) {
         Node temp = new Node(val);
-        if(head==null) head = tail = temp;
+        if (head == null) head = tail = temp;
         else {
             temp.next = head;
             head = temp;
         }
+        size++;
     }
 
-    void deleteAtHead(){
-        if(head == null) {
+    void deleteAtHead() {
+        if (head == null) {
             System.out.println("List is Empty");
             return;
         }
         head = head.next;
+        if (head == null) tail = null;
+        size--;
+    }
+
+    int get(int idx) {
+        Node temp = head;
+        for (int i = 1; i <= idx; i++) {
+            temp = temp.next;
+        }
+        return temp.val;
     }
 
     void display() {
@@ -64,6 +111,8 @@ public class LinkedListDataStructure {
         ll.addAtHead(55);
         ll.display();
         ll.deleteAtHead();
+        ll.display();
+        ll.addAtIndex(4, 99);
         ll.display();
     }
 }
